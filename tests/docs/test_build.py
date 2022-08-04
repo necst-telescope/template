ee56f538d7763ad7a94dc1ebae592a20b5279a3a
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-08-04 14:01:50 +0900

    Omit stderr
    Since it can be used by project logger

--- tests/docs/test_build.py
+++ tests/docs/test_build.py
@@ -43,7 +43,6 @@ def test_create_stub(tmp_project_dir: Path):
         capture_output=True,
     )
     assert result.returncode == 0
-    assert result.stderr == b""
 
 
 @pytest.mark.skipif(
@@ -67,5 +66,4 @@ def test_build(tmp_project_dir: Path):
     )
     print(result.stderr, result.stdout)
     assert result.returncode == 0
-    assert result.stderr == b""
 
